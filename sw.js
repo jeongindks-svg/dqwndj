6e04adb810b1b21e55bfcdc046638825e1d5f259
jeongindks-svg <jeongindks@gmail.com>
2026-09-25 20:34:37 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="jubaedang-v1";
+const FILES=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
+  }).catch(()=>caches.match("./index.html"))));
+});
